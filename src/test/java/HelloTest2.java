08669487ba4464b8962a1c602beb092d42dc6b99
Rohitupe <rohittupe21@gmail.com>
2021-02-02 21:33:19 +0530

    run code without specifying driver path

---
+++ src/test/java/HelloTest2.java
@@ -0,0 +1,23 @@
+import org.junit.BeforeClass;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/* Run Web Driver without specifying path */
+public class HelloTest2 {
+
+    // This method will run once before all the tests in our class
+    @BeforeClass
+    public static void setUpClass(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    // This is the test method where test cases are specified
+    @Test
+    public void FirstTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
+}
