a0e9c21112bc2e9a2475d711257713ad80f0bd5e
Rohitupe <rohittupe21@gmail.com>
2021-02-02 21:11:53 +0530

    run driver by specifying path

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,2 +1,18 @@
+// Test should be written at the end of every class - to run test with maven
+
+/* Here we have to specify driver manually and have to run it by specifying the path of driver */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HelloTest {
+
+    // writing first test
+    public void firstTest(){
+        //Telling the system where to find chromedriver on Windows.
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+    }
 }
